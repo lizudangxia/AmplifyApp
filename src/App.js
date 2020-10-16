8dec9a85c13f04b5dd4d8ee61623646eeb9096bd
lizudangxia <69703892+lizudangxia@users.noreply.github.com>
2020-10-16 21:24:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.xuxi.cn"
           target="_blank"
           rel="noopener noreferrer"
         >
